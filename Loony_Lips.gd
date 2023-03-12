e6e35032d7c97ee8d9306aa6e7cdcee046a3a3f9
najongjine <54340913+najongjine@users.noreply.github.com>
2023-03-12 13:19:35 +0900

    .

--- Loony_Lips.gd
+++ Loony_Lips.gd
@@ -1,23 +1,53 @@
 extends Control
 
+var player_words=[]
+var story="Once upon a time %s watched %s and was it's %s movie of %s year."
+var prompts=["a name","a noun","adverb","adjective"]
+
+@onready var PlayerText=$VBoxContainer/HBoxContainer/PlayerText
+@onready var DisplayText=$VBoxContainer/DisplayText
+@onready var LableForButtonText=$VBoxContainer/HBoxContainer/LabelForButton
 
 func _ready():
-	var prompts=["Yann","Minions","greatest","2000"]
-	var story="Once upon a time %s watched %s and was it's %s movie of %s year."
-	print(story %prompts)
+	DisplayText.text="Welcome ! "
+	check_player_words_length()
+	PlayerText.grab_focus()
 
-	$VBoxContainer/DisplayText.text= story %prompts
-
-
-func _on_PlayerText_text_entered(new_text):
-	update_DisplayText(new_text)
-	
 func _on_TextureButton_pressed():
-	var words=$VBoxContainer/HBoxContainer/PlayerText.text
-	update_DisplayText(words)
+	if is_story_done():
+		get_tree().reload_current_scene()
+	else:
+		add_to_player_words()
 
-func update_DisplayText(new_text):
-	$VBoxContainer/DisplayText.text= new_text
-	$VBoxContainer/HBoxContainer/PlayerText.clear()
+func add_to_player_words():
+	player_words.append(PlayerText.text)
+	DisplayText.text=""
+	PlayerText.clear()
+	check_player_words_length()
+
+func is_story_done():
+	return player_words.size() == prompts.size()
+
+func check_player_words_length():
+	if is_story_done():
+		end_game()
+	else:
+		prompt_player()
+
+func tell_story():
+	DisplayText.text=story %player_words
+	
+func prompt_player():
+	DisplayText.text+="May I have "+prompts[player_words.size()] + " please?"
 
 
+func _on_player_text_text_submitted(new_text):
+	if is_story_done():
+		end_game()
+	else:
+		add_to_player_words()
+	
+func end_game():
+	PlayerText.queue_free()
+	tell_story()
+	LableForButtonText.text="Again"
